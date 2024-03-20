34e09fdb1f8e665ed706af14a5f1448891e6fdab
maximoortelli <maximoortelli@hotmail.com>
2024-03-20 13:56:31 -0300

    Added all files
    Added all files

---
+++ app/types.tsx
@@ -0,0 +1,16 @@
+//LAYOUT INCLUDE TYPES
+export interface RandomUsers {
+  id: string;
+  name: string;
+  image: string;
+}
+
+export interface MenuItemsTypes {
+  iconString: string;
+  colorString: string;
+  sizeString: string;
+}
+
+export interface MenuItemFollowCompTypes {
+  user: RandomUsers;
+}
